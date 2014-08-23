75a7589fbb0a1b42a650ed18f20b34d5a74dc809
Colder Xihk <cncolder@gmail.com>
2014-08-23 13:38:28 +0800

    make mocha use traceur as a compiler.
    just like coffee.

--- test/helpers/first.js
+++ test/helpers/first.js
@@ -1,29 +1,13 @@
-const File    = require('fs');
-const Module  = require('module');
 const traceur = require('traceur');
 
 
 // All JS files, excluding node_modules, are transpiled using Traceur.
-const originalRequireJs = Module._extensions['.js'];
-Module._extensions['.js'] = function(module, filename) {
-  if (/\/(node_modules|test\/scripts)\//.test(filename)) {
-    return originalRequireJs(module, filename);
-  } else {
-    const source = File.readFileSync(filename, 'utf8');
-    const compiled = traceur.compile(source, {
-      blockBinding:           true,
-      generators:             true,
-      generatorComprehension: true,
-      asyncFunctions:         true,
-      validate:     true,
-      filename:     filename,
-      sourceMap:    true
-    });
-    if (compiled.errors.length)
-      throw new Error(compiled.errors.join('\n'));
-    // No idea when/why Traceur adds an empty export
-    const cleaned = compiled.js.replace(/module.exports = {};/g, '');
-    return module._compile(cleaned, filename);
-  }
-};
+traceur.require.makeDefault(function(filename) {
+  return !(/\/(node_modules|test\/scripts)\//.test(filename));
+}, {
+  // for traceur >= 0.0.51
+  experimental: true
+});
 
+// for traceur < 0.0.50, not work in the new version.
+traceur.options.experimental = true;
