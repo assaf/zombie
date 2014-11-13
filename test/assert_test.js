ccaf7e18b8e11d5d1f68f751e75c34af0e68ac8e
Assaf Arkin <assaf@labnotes.org>
2014-11-13 15:49:01 -0800

    More test cases are now ES7

---
+++ test/assert_test.js
@@ -0,0 +1,65 @@
+const assert      = require('assert');
+const Browser     = require('../src/zombie');
+const { brains }  = require('./helpers');
+
+
+describe('Browser assert', function() {
+  let browser;
+
+  before(function() {
+    browser = Browser.create();
+    return brains.ready();
+  });
+
+
+  describe('link', function() {
+    before(function() {
+      brains.static('/assert/link', `
+        <!DOCTYPE html>
+        <html lang=en>
+          <head><title>test</title></head>
+          <body>
+            <div>
+              <p id="p-id">
+                <a href="/assert/link/link-to-some-id-12345" id="link-id">Link Text</a>
+              </p>
+            </div>
+          </body>
+        </html>
+      `);
+      brains.static('/assert/link/link-to-some-id-12345', `
+        <html>
+          <body>
+          </body>
+        </html>
+      `);
+    });
+
+
+    before(function() {
+      return browser.visit('/assert/link');
+    });
+
+    it('should find the link using a wide selector', function() {
+      browser.assert.link('a', 'Link Text', '/assert/link/link-to-some-id-12345');
+    });
+
+    it('should find the link using a specific selector', function() {
+      browser.assert.link('div p a', 'Link Text', '/assert/link/link-to-some-id-12345');
+    });
+
+    it('should find the link using an id selector', function() {
+      browser.assert.link('#link-id', 'Link Text', '/assert/link/link-to-some-id-12345');
+    });
+
+    it('should find the link when given a RegExp for the url', function() {
+      browser.assert.link('#link-id', 'Link Text', /\/assert\/link\/link\-to\-some\-id\-\d*$/);
+    });
+  });
+
+
+  after(function() {
+    browser.destroy();
+  });
+});
+
---
+++ test/event_source_test.js
@@ -0,0 +1,65 @@
+const assert      = require('assert');
+const Browser     = require('../src/zombie');
+const { brains }  = require('./helpers');
+
+
+describe('EventSource', function() {
+  let browser;
+
+  before(function() {
+    browser = Browser.create();
+    return brains.ready();
+  });
+
+  before(function() {
+    brains.static('/streaming', `
+      <html>
+        <head>
+          <script>
+            var source = new EventSource("/stream");
+            window.events = [];
+            source.addEventListener("test", function(event) {
+              window.events.push(event.data)
+            });
+          </script>
+        </head>
+      </html>
+    `);
+
+    brains.get('/stream', function(req, res) {
+      res.writeHead(200, {
+        'Content-Type':   'text/event-stream; charset=utf-8',
+        'Cache-Control':  'no-cache',
+        'Connection':     'keep-alive'
+      });
+      res.write('event: test\nid: 1\ndata: first\n\n');
+      setTimeout(function() {
+        res.write('event: test\nid: 2\ndata: second\n\n');
+        res.end();
+      }, 100);
+    });
+  });
+
+  before(function(done) {
+    browser.visit('/streaming');
+
+    function gotBothEvents(window) {
+      return (window.events && window.events.length === 2);
+    }
+
+    browser.wait(gotBothEvents, ()=> {
+      this.events = browser.evaluate('window.events');
+      done();
+    });
+  });
+
+  it('should stream to browser', function() {
+    assert.deepEqual(this.events, ['first', 'second']);
+  });
+
+  after(function() {
+    // NOTE this blows up
+    // browser.destroy();
+  });
+});
+
