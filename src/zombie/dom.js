c6a66f83916012737bbda87b2212f3aa005f4b66
Assaf Arkin <assaf@labnotes.org>
2015-02-12 12:55:13 -0800

    Moved JSDOM patches to dom.js

--- src/zombie/dom.js
+++ src/zombie/dom.js
@@ -1,6 +1,8 @@
 // Exports the JSDOM DOM living namespace.
 
-const DOM         = require('jsdom/lib/jsdom/living');
+const DOM = require('jsdom/lib/jsdom/living');
+module.exports = DOM;
+
 
 // Additional error codes defines for XHR and not in JSDOM.
 DOM.SECURITY_ERR  = 18;
@@ -8,4 +10,11 @@ DOM.NETWORK_ERR   = 19;
 DOM.ABORT_ERR     = 20;
 DOM.TIMEOUT_ERR   = 23;
 
-module.exports = DOM;
+
+// Monkey patching JSDOM.  This is why we can't have nice things.
+require('./jsdom_patches');
+require('./forms');
+require('./dom_focus');
+require('./dom_iframe');
+require('./scripts');
+
