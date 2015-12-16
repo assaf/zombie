9a6a6c4a7cef4a18474dbba953d834e5b9eca52e
Anton <anton.dmukhovskiy@digitalanimal.com>
2015-12-16 20:12:52 +0000

    Update test to include a form

--- test/window_test_2.js
+++ test/window_test_2.js
@@ -32,7 +32,11 @@ describe('My Window', async function() {
         <title>Opened Window</title>
       </head>
       <body>
-        <a href="/window/navigate" id="navigate">navigate</a>
+        <form action="/window/opened/echo">
+          <input type="text" name="name"></input>
+          <input type="submit" value="submit">
+        </form>
+        <a href="/window/navigated" id="navigate">navigate</a>
         <a href="#" id="close">close</a>
         <script>
           document.getElementById('close').onclick = function(){
@@ -42,7 +46,18 @@ describe('My Window', async function() {
       </body>
       </html>
     `);
-    brains.static('/window/navigate', `
+    brains.static('/window/opened/echo', `
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>Opened Window - Submitted form</title>
+      </head>
+      <body>
+        echo
+      </body>
+      </html>
+    `);
+    brains.static('/window/navigated', `
       <!DOCTYPE html>
       <html>
       <head>
@@ -57,21 +72,54 @@ describe('My Window', async function() {
     await browser.visit('/window/initial');
   });
   
-  it('should open a new window', async function(){
-    assert(browser.tabs.length == 1);
+  
+  it('should open a new window and switch to it', async function(){
+    browser.tabs.closeAll();
+    await browser.visit('/window/initial');
+    const initialWindow = browser.window;
+    
     await browser.clickLink('#open');
-    assert(browser.tabs.length == 2);
-  });
-  it('should remember window\'s opener', function(){
-    assert(browser.tabs[1].opener == browser.tabs[0]);
-  });
-  it('should switch eventLoop.active to the new tab', function(){
-    assert(browser._eventLoop.active == browser.tabs[1]);
-  });
-  it('should remember the opener after navigating the window', async function(){
-    //console.log(browser.window);
+    assert(browser.tabs.length == 2); //should open a new tab
+    assert(/window\/opened$/.test(browser.window._request.url));
+    assert(browser.window.opener == initialWindow);
+    assert(browser._eventLoop.active == browser.window);
+  })
+  
+  it('should remember the opener after clicking on a link in an opened window', async function(){
+    browser.tabs.closeAll();
+    await browser.visit('/window/initial'); //start over
+    const initialWindow = browser.window;
+    
+    await browser.clickLink('#open');
+    const openedWindow = browser.window;
+    
     await browser.clickLink('#navigate');
     assert(browser.tabs.length == 2); //should not open a new tab
+    assert(/window\/navigated$/.test(browser.window._request.url));
+    assert(browser.window._history.first.window == openedWindow);
+    assert(browser.window._history.current.window == browser.window);
+    assert(browser._eventLoop.active == browser.window);
+    assert(browser.window.opener == initialWindow); //fail!
+    
   });
+  
+  it('should remember the opener after submiting a form in the window', async function(){
+    browser.tabs.closeAll();
+    await browser.visit('/window/initial'); //start over
+    const initialWindow = browser.window;
+    
+    await browser.clickLink('#open');
+    const openedWindow = browser.window;
+    browser.fill('name', 'Zombie');
+    
+    await browser.pressButton('submit');
+    assert(browser.tabs.length == 2); //should not open a new tab
+    assert(/window\/opened\/echo/.test(browser.window._request.url));
+    assert(browser.window._history.first.window == openedWindow);
+    assert(browser.window._history.current.window == browser.window);
+    assert(browser.window.opener == initialWindow); //pass!
+    
+  });
+  
 });
 
