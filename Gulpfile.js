6a58f133c17ce21004765c353e399f9a37c1ab9b
Assaf Arkin <assaf@labnotes.org>
2018-11-27 09:45:24 -0800

    Use Gulp 4.x

--- Gulpfile.js
+++ Gulpfile.js
@@ -10,21 +10,20 @@ const sourcemaps  = require('gulp-sourcemaps');
 const babel       = require('gulp-babel');
 
 
-// gulp -> gulp watch
-gulp.task('default', ['watch']);
-
-
-// gulp lint -> errors if code dirty
-gulp.task('lint', function () {
+function lint() {
   return gulp.src([ 'src/**/*.js', 'test/*.js' ])
     .pipe(eslint())
     .pipe(eslint.formatEach())
     .pipe(eslint.failOnError());
-});
+}
 
 
-// gulp build -> compile coffee script
-gulp.task('build', ['clean'], function() {
+function clean() {
+  return del('lib/**');
+}
+
+
+function build() {
   return gulp.src('src/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(babel())
@@ -34,38 +33,17 @@ gulp.task('build', ['clean'], function() {
       message: 'Zombie: built!',
       onLast:  true
     }));
-});
+}
 
 
-// gulp clean -> clean generated files
-gulp.task('clean', function() {
-  return del('lib/**');
-});
+function watch() {
+  return gulp.watch('src/*.js', gulp.series(clean, build));
+}
 
-
-// gulp watch -> watch for changes and compile
-gulp.task('watch', ['build'], function() {
-  return gulp.watch('src/*.js', ['clean', 'build']);
-});
-
-
-// gulp tag -> Tag this release
-gulp.task('tag', ['changes'], function() {
-  const version = require('./package.json').version;
-  const tag     = 'v' + version;
-
-  gutil.log('Tagging this release', tag);
-  return gulp.src('.changes')
-    .pipe( exec('git add package.json CHANGELOG.md') )
-    .pipe( exec('git commit --allow-empty -m "Version ' + version + '"') )
-    .pipe( exec('git tag ' + tag + ' --file .changes') )
-    .pipe( exec('git push origin ' + tag) )
-    .pipe( exec('git push origin master') );
-});
 
 // Generate a change log summary for this release
 // git tag uses the generated .changes file
-gulp.task('changes', function() {
+function changes() {
   const version   = require('./package.json').version;
   const changelog = File.readFileSync('CHANGELOG.md', 'utf-8');
   const match     = changelog.match(/^## Version (.*) .*\n([\S\s]+?)\n##/m);
@@ -76,5 +54,33 @@ gulp.task('changes', function() {
   const changes   = match[2].trim();
   assert(changes, 'CHANGELOG.md empty entry for version ' + version);
   File.writeFileSync('.changes', changes);
-});
+}
+
+
+function tag() {
+  const version = require('./package.json').version;
+  const tag     = 'v' + version;
+
+  gutil.log('Tagging this release', tag);
+  return gulp.src('.changes')
+    .pipe( exec('git add package.json CHANGELOG.md') )
+    .pipe( exec('git commit --allow-empty -m "Version ' + version + '"') )
+    .pipe( exec('git tag ' + tag + ' --file .changes') )
+    .pipe( exec('git push origin ' + tag) )
+    .pipe( exec('git push origin master') );
+}
+
+
+// gulp build -> compile coffee script
+exports.build = gulp.series(clean, build, lint);
+// gulp clean -> clean generated files
+exports.clean = clean;
+// gulp lint -> errors if code dirty
+exports.lint = lint;
+// gulp watch -> watch for changes and compile
+exports.watch = gulp.series(build, watch);
+// gulp tag -> Tag this release
+exports.tag = gulp.series(changes, tag);
+// gulp -> gulp watch
+exports.default = exports.watch;
 
